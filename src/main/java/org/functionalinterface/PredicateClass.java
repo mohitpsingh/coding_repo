bc3e04d98d4e308a9e2f29479046b47bb865b484
CoderX13 <91383422+CoderX13@users.noreply.github.com>
2024-04-07 23:59:35 +0530

    Predicate

--- src/main/java/org/functionalinterface/PredicateClass.java
+++ src/main/java/org/functionalinterface/PredicateClass.java
@@ -1,4 +1,25 @@
 package org.functionalinterface;
 
+import org.streamsapi.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class PredicateClass {
+
+    public static List<Student> students = new ArrayList<>();
+    public static void main(String[] args) {
+
+
+        students.add(new Student("Mohit", "Singh", "mohitsingh@gmail.com", "9999888898", 23, "BTech"));
+        students.add(new Student("Karan", "Kumar", "karankumar@gmail.com", "9994588898", 20, "MBBS"));
+        students.add(new Student("Rohit", "Singh", "rohit@gmail.com", "9999888898", 26, "BTech"));
+        students.add(new Student("Raghav", "Roy", "raghavroy@gmail.com", "89999888898", 25, "MBA"));
+        students.add(new Student("Raman", "Mehta", "ramanmehta@gmail.com", "7999888848", 22, "MCA"));
+
+        Predicate<Student> agePredicate = student -> student.getAge() > 23;
+
+
+    }
 }
